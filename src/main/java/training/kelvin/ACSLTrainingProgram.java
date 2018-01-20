5a797c1ec7658dc8b27829988e62d9f1b630c042
Kelvin Yuan <kelvinyuan2004@gmail.com>
2018-01-19 19:31:23 -0500

    testing

--- src/main/java/training/kelvin/ACSLTrainingProgram.java
+++ src/main/java/training/kelvin/ACSLTrainingProgram.java
@@ -9,6 +9,7 @@ public class ACSLTrainingProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.printIn("I'm Kelvin");
 	}
 
 }
