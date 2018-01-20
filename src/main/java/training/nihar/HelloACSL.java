7d7758405317b46f83e98741c2f21a438b94e5b8
Nihar Xavier <nihar.xavier@gmail.com>
2018-01-19 19:24:01 -0500

    this is my first java program

---
+++ src/main/java/training/nihar/HelloACSL.java
@@ -0,0 +1,10 @@
+package training.nihar;
+
+public class HelloACSL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
