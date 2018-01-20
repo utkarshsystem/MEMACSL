bead1bf603733270160bfb07d132c9737001924e
LAPTOP-UQR13TP0\haric <ashritacr90@gmail.com>
2018-01-19 19:29:19 -0500

    Testing

--- src/main/java/training/ashrita/HelloACSL.java
+++ src/main/java/training/ashrita/HelloACSL.java
@@ -5,10 +5,12 @@ public class HelloACSL {
 	public HelloACSL() {
 		// TODO Auto-generated constructor stub
 	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello acsl");
+		system.out ("I am Ashrita");
 	}
 
 }
